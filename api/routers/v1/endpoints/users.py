54fd79ab049ef91b76de534f320ffe8946193e47
Jorge Massih <jorgmassih@gmail.com>
2023-05-22 13:55:51 -0400

    🐛 fix user info dependencies

--- api/routers/v1/endpoints/users.py
+++ api/routers/v1/endpoints/users.py
@@ -1,8 +1,7 @@
-from api.security import auth, hashing, jwt_handler as jwt
 from api.pydantic_models.user import UserInfoPydantic
-from api.routers.v1.dependencies import token_dep
+from api.routers.v1.dependencies import current_user
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter
 from fastapi.security import OAuth2PasswordBearer
 
 from typing import Annotated
@@ -11,22 +10,6 @@ from typing import Annotated
 router = APIRouter()
 
 @router.get("/me", response_model=UserInfoPydantic)
-async def get_user_info(token: token_dep):
-    credentials_exception = HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials.",
-            headers={"WWW-AUTHENTICATE": "Bearer"}
-            )
-    try:
-        username = jwt.decode_username(token)
-        if not username:
-            raise credentials_exception
-    except Exception:
-        raise credentials_exception
-
-    user = await auth.get_user(username=username)
-    if not user:
-        raise credentials_exception
-    return user
-
+def get_user_info(current_user: current_user):
+    return current_user
 

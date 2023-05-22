00ac10640f353400c9db332807d491d98dd62825
Jorge Massih <jorgmassih@gmail.com>
2023-05-22 02:43:19 -0400

    :bug: fix oauth_scheme tokenUrl path

--- api/main.py
+++ api/main.py
@@ -1,10 +1,10 @@
 from api.database.settings import TORTOISE_ORM
-from api.routers.v1.api import api_router as api_v1
+from api.routers.v1 import api as v1, ROUTE_PREFIX as v1_prefix
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
 
 app = FastAPI()
-app.include_router(api_v1, prefix="/api/v1")
+app.include_router(v1.router, prefix=v1_prefix)
 
 # Health Check
 @app.get("/ping", tags=["Health Check"])
--- api/routers/v1/api.py
+++ api/routers/v1/api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from api.routers.v1.endpoints import login, users 
 
-api_router = APIRouter()
-api_router.include_router(login.router, tags=['User'])
-api_router.include_router(users.router, prefix='/users', tags=['User'])
+router = APIRouter()
+router.include_router(login.router, tags=['User'])
+router.include_router(users.router, prefix='/users', tags=['User'])
 

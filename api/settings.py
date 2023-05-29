f18ad94d3df5a1ff10b52994bd7faafb677354ba
Jorge Massih <jorgmassih@gmail.com>
2023-05-29 13:41:15 -0400

    🔧 set variables as optional

--- api/settings.py
+++ api/settings.py
@@ -12,20 +12,20 @@ class Settings(BaseSettings):
     DATABASE_HOST: str
     DATABASE_NAME: str
     DATABASE_PORT: int | str
-    TORTOISE_ORM: dict | None
+    TORTOISE_ORM: dict = None
 
     ACCESS_TOKEN_SECRET_KEY: str
     ACCESS_TOKEN_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
-    SENDGRID_API_URL: HttpUrl
-    SENDGRID_API_KEY: str
-    SENDGRID_SENDER: EmailStr
-    SENDGRID_NEW_USER_TEMPLATE_ID: str
+    SENDGRID_API_URL: HttpUrl = None
+    SENDGRID_API_KEY: str = None
+    SENDGRID_SENDER: EmailStr = None
+    SENDGRID_NEW_USER_TEMPLATE_ID: str = None
 
-    TWILIO_FROM_NUMBER: str
-    TWILIO_API_URL: HttpUrl
-    TWILIO_API_KEY: str
+    TWILIO_FROM_NUMBER: str = None
+    TWILIO_API_URL: HttpUrl = None
+    TWILIO_API_KEY: str = None
 
     @validator("TORTOISE_ORM", always=True)
     def create_tortoise_config(cls, v, values, **kwargs):

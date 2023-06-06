37f3c9242fd60d20b7a2fb042fc5032c272e2160
Jorge Massih <jorgmassih@gmail.com>
2023-06-06 12:10:52 -0400

    🥅 add db error handler in api initiation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from app.core.settings import TORTOISE_ORM, init_loggers
+from app.core.settings import TORTOISE_ORM, init_loggers, log
 from app.routers.v1 import api as v1, ROUTE_PREFIX as v1_prefix
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
@@ -15,7 +15,13 @@ try:
         config=TORTOISE_ORM,
         add_exception_handlers=True,
     )
-except db_exception:
+except db_exception.ConfigurationError as e:
+    log.error("A config error has occurred: {e}")
+    raise e
+
+except db_exception.DBConnectionError as e:
+    log.error("A connection error has occurred: {e}")
+    raise e
 
 app.include_router(v1.router, prefix=v1_prefix)
 

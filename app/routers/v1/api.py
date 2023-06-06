c239fff54e52ff2757146fcdcfd7ea0cf21bc593
Jorge Massih <37001774+plusiv@users.noreply.github.com>
2023-06-06 11:55:27 -0400

    🚚 rename endopoints modules names

--- app/routers/v1/api.py
+++ app/routers/v1/api.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from fastapi import APIRouter
-from app.routers.v1.endpoints import login, users, email, sms, todos
+from app.routers.v1.endpoints import login, users, emails, sms, todos
 
 router = APIRouter()
 router.include_router(login.router, tags=["User"])
 router.include_router(users.router, prefix="/users", tags=["User"])
-router.include_router(email.router, prefix="/email", tags=["Emails"])
-router.include_router(sms.router, prefix="/sms", tags=["SMS"])
 router.include_router(todos.router, prefix="/todos", tags=["To Dos"])
+router.include_router(emails.router, prefix="/emails", tags=["Emails"])
+router.include_router(sms.router, prefix="/sms", tags=["SMS"])
